024d69f3de26d63185910c52ec0e76074b11382f
GuadalupeVeliz <guadalupeveliz100@gmail.com>
2026-07-11 07:24:01 -0300

    correcion

--- swagger.js
+++ swagger.js
@@ -12,7 +12,7 @@ const doc = {
   },
   host: "proybackendgrupo01-ecl4.onrender.com",
   basePath: "/",
-  schemes: ["http", "https"],
+  schemes: ["https", "http"],
   tags: [
     { name: "Auth", description: "Login, registro y autenticación con Google" },
     {
